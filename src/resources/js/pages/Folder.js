4f12e02d9c61e533605a94065a75dc76e066b4c0
yu-chap <yuppy4676@gmail.com>
2022-06-04 22:49:51 +0900

    (#18) modified Folder.js

--- src/resources/js/pages/Folder.js
+++ src/resources/js/pages/Folder.js
@@ -4,15 +4,10 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import ItemManagement from '../components/ItemManagement/ItemManagement';
 
-// /app/home/:id/itemsの画面へ出力する要素 //
-// idによって選択されたフォルダに関連するItems一覧を表示
+// /app/home/:folderId/itemsの画面へ出力する要素 //
+// folderIdによって選択されたフォルダに関連するItems一覧を表示
 const Folder = () => {
     const { folderId } = useParams();
-    const [items, setItems] = useState(null);
-
-    useEffect(() => {
-        setItems(folderId);
-    }, [folderId])
 
     return (
         <Box sx={{ display: "flex", justifyContent: "center" }}>

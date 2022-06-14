44b0b2294b4ab92d5075fbbbf6bfaa103628d625
yu-chap <yuppy4676@gmail.com>
2022-06-14 19:05:51 +0900

    (#50) create error page

---
+++ src/resources/views/errors/403.blade.php
@@ -0,0 +1,16 @@
+@include('layouts.head')
+<div class="bg-black bg-gradient" style="width: 100%; height: 100vh; background-size: cover; background-position: center;">
+    <div class="container d-flex align-items-center justify-content-center" style="width: 100%; height: 100vh;">
+        <div class="row d-flex align-items-center justify-content-center">
+            <div class="col w-100 d-flex align-items-center justify-content-center">
+                <h3 class="text-center text-white">お探しのページにアクセスする権限がありません。</h3>
+            </div>
+            <div class="w-100"></div>
+            <div class="col w-100 d-flex align-items-center justify-content-center mt-3">
+                <a href="{{ route('top') }}" class="btn btn-warning btn-lg" role="button">
+                    ホーム
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ src/resources/views/layouts/head.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    @if(config('app.env') === 'production')
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    @else
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    @endif
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    @if(config('app.env') === 'production')
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    @else
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @endif
+</head>
